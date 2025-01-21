307322ece4c30d188475efee0e047e3878fa189f
devpresss xmont <xmontemorjerald@gmail.com>
2025-01-22 00:31:58 +0800

    Added animation in car(speed)

--- kamoterider.js
+++ kamoterider.js
@@ -1,39 +1,64 @@
-
 // Get the canvas element
 const canvas = document.getElementById('gameCanvas');
 
 // Get the canvas context
-
 const ctx = canvas.getContext('2d');
 
+// Set the car x position
+let carX = canvas.width / 2 - 10;
 
-//Road image
-const road = new Image();   
+// Set the car y position
+let carY = canvas.height - 30;
 
-//Set the source of the image
-road.src="images/road.png";
+// Car height
+let carHeight = 30;
 
+// Car width
+let carWidth = 20;
 
-//Draw the image on the canvas
-road.onload = function(){
-    ctx.drawImage(road,0,0,canvas.width,canvas.height);
+// Car speed
+let carSpeed = 1;
 
-    //Car image
-    const car = new Image();
+// Load road image
+const road = new Image();
+road.src = "images/road.png";
 
-    car.src = "images/Car1.png";
+// Load car image
+const car = new Image();
+car.src = "images/Car1.png";
 
-
-
-    //Draw the car on the canvas
-    car.onload = function(){
-
-
-        //Position of the car
-        
-        ctx.drawImage(car,canvas.width/2-10,canvas.height-30,20,30);
-    }
+// Draw road function
+function drawRoad() {
+    ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
 }
 
+// Draw car function
+function drawCar() {
+    ctx.drawImage(car, carX, carY, carWidth, carHeight);
+}
 
+// Animation loop
+function animate() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Draw the road image
+    drawRoad();
+
+    // Update car position
+    carY -= carSpeed;
+
+    // Reset car position if it goes off screen
+    if (carY < -carHeight) {
+        carY = canvas.height;
+    }
+
+    // Draw the car
+    drawCar();
+
+    // Request next frame
+    requestAnimationFrame(animate);
+}
+
+// Start animation when images are loaded
+road.onload = car.onload = animate;
